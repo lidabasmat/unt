4110347f12b5210cb117fa3ffdf326e70d388f1b
Lidiia Basmat <lida.basmat@gmail.com>
2022-06-09 23:17:51 +0300

    add nav mobile

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,80 @@
+import classnames from 'classnames';
+import React from 'react';
+import { Link } from '../Link';
+
+import PropTypes from '../../utils/PropTypes';
+import warning from '../../utils/warning';
+
+const propTypes = {
+    children: PropTypes.node,
+    className: PropTypes.string,
+    variant: PropTypes.oneOf(['reset', 'primary']),
+    size: PropTypes.oneOf(['reset', 'md']),
+    submit: PropTypes.bool,
+    onClick: PropTypes.func,
+    to: PropTypes.string,
+};
+
+const defaultProps = {
+    children: null,
+    className: null,
+    variant: 'reset',
+    size: 'reset',
+    submit: false,
+    onClick: null,
+    to: null,
+};
+
+const Button = ({
+    children,
+    className,
+    onClick,
+    submit,
+    variant,
+    size,
+    to,
+    ...rest
+}) => {
+    const buttonClassName = classnames(
+        className,
+        'leading-none antialiased select-none transition-all duration-200',
+        {
+            'bg-transparent': variant === 'reset',
+            'btn-primary': variant === 'primary',
+            'px-0 py-0': size === 'reset',
+            'px-8 py-4 text-base': size === 'md',
+        },
+    );
+
+    warning(
+        !(submit && onClick),
+        'You provide onClick to a submit button. Please remove onClick handler or submit attribute',
+    );
+
+    if (submit) {
+        return (
+            <button type="submit" className={buttonClassName}>
+                {children}
+            </button>
+        );
+    }
+
+    if (to) {
+        return (
+            <Link to={to} {...rest}>
+                {children}
+            </Link>
+        );
+    }
+
+    return (
+        <button type="button" onClick={onClick} className={buttonClassName}>
+            {children}
+        </button>
+    );
+};
+
+Button.propTypes = propTypes;
+Button.defaultProps = defaultProps;
+
+export default Button;
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,34 +1,52 @@
-import * as React from 'react';
+import classnames from 'classnames';
+import React, { useState } from 'react';
 
+import Button from '../components/Button';
 import Link from '../components/Link';
+import MenuIcon from '../components/icons/MenuIcon';
 
-import IconCartLogo from '../images/logo.svg';
+import Logo from '../images/logo.svg';
 
 const Header = () => {
+  const [ opened, setOpened ] = useState(false);
   return (
     <>
-      <header className="bg-red-500 py-5">
+      <header className="bg-red-500 py-5 text-white">
         <div className="container-lg">
           <div className="flex justify-between">
             <Link to="/">
-              <img src={IconCartLogo} alt="logo" />
+              <img src={Logo} alt="logo" />
             </Link>
             <nav className="hidden md:flex items-center">
-              <Link to="/pisni/" className="typo-intro text-white">
+              <Link to="/pisni/" className="typo-intro">
                 пісні
               </Link>
               <div className="w-2 h-2 mx-8 bg-white" />
-              <Link to="/smikhovyny/" className="typo-intro text-white">
+              <Link to="/smikhovyny/" className="typo-intro">
                 сміховини
               </Link>
               <div className="w-2 h-2 mx-8 bg-white" />
-              <Link to="/categories/" className="typo-intro text-white">
+              <Link to="/categories/" className="typo-intro">
                 категорії
               </Link>
             </nav>
+            <div className="md:hidden">
+              <Button onClick={() => { setOpened(!opened) }}>
+                <MenuIcon />
+              </Button>
+            </div>
           </div>
         </div>
       </header>
+      <div className={classnames(
+        'fixed top-0 h-screen right-0 max-w-full w-[260px] bg-black transition-all duration-200',
+        {
+          'opacity-100 visible': opened,
+          'opacity-0 invisible': !opened,
+        },
+      )}
+      >
+      </div>
     </>
   );
 };
